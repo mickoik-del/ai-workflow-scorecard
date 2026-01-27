c6f15e4bed92684f83619d4986cedb7ecd7ee528
mickoik-del <michael.oiknine@gmail.com>
2026-01-27 17:17:36 +0100

    Update submit-to-hubspot.js

--- api/submit-to-hubspot.js
+++ api/submit-to-hubspot.js
@@ -1,4 +1,4 @@
-export default async function handler(req, res) {
+module.exports = async (req, res) => {
     // Only allow POST requests
     if (req.method !== 'POST') {
         return res.status(405).json({ message: 'Method Not Allowed' });
